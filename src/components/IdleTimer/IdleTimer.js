683b5056251e2e7e1b4e37d55ade9c7ff1365325
Mike Taylor <mike@indexdata.com>
2018-09-10 17:48:48 +0100

    Fix ESLint errors

--- src/components/IdleTimer/IdleTimer.js
+++ src/components/IdleTimer/IdleTimer.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import IdleTimer from 'react-idle-timer';
-import ReactDOM from 'react-dom';
 
-function idleTimer(onActive,onIdle,timeout,rootdocument) {
+function idleTimer(onActive, onIdle, timeout, rootdocument) {
   return (<IdleTimer
-      element={rootdocument}
-      onActive={onActive}
-      onIdle={onIdle}
-      timeout={timeout}
-      startOnMount
-    />);
-
+    element={rootdocument}
+    onActive={onActive}
+    onIdle={onIdle}
+    timeout={timeout}
+    startOnMount
+  />);
 }
 
 export default idleTimer;
