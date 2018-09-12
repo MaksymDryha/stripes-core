22a916abd9e79104ea86ff41f8eecb48b288e3b7
Mike Taylor <mike@indexdata.com>
2018-09-12 11:22:32 +0100

    Omit "mousemove" event from idle-timer
    Fixes STCOR-252.

--- src/components/IdleTimer/IdleTimer.js
+++ src/components/IdleTimer/IdleTimer.js
@@ -2,7 +2,25 @@ import React from 'react';
 import IdleTimer from 'react-idle-timer';
 
 function idleTimer(onActive, onIdle, timeout, rootdocument) {
+  // List of events copied from
+  // https://github.com/SupremeTechnopriest/react-idle-timer/blob/468b95ee86f1401c15ee0bb845cc08c2b731db69/src/index.js#L36-L47
+  // and the unwanted 'mousemove' removed.
+  // There seems to be no more elegant way to do this.
+  const EVENTS = [
+    // 'mousemove',
+    'keydown',
+    'wheel',
+    'DOMMouseScroll',
+    'mouseWheel',
+    'mousedown',
+    'touchstart',
+    'touchmove',
+    'MSPointerDown',
+    'MSPointerMove'
+  ];
+
   return (<IdleTimer
+    events={EVENTS}
     element={rootdocument}
     onActive={onActive}
     onIdle={onIdle}
